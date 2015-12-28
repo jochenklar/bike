166bce60d6cb00736818c928d11acef007135cd0
Jochen Klar <jochenklar@gmail.com>
2015-12-28 13:16:21 +0100

    django 1.9 and file organization

--- bike/views.py
+++ bike/views.py
@@ -1,3 +1,4 @@
+from django.conf import settings
 from django.http import HttpResponse
 from django.shortcuts import render,redirect
 
@@ -7,8 +8,14 @@ def index(request):
     # get tracks
     years = range(2009,2015)
     tracks = {}
-    for year in years:  
+    for year in years:
         tracks[year] = Track.objects.filter(timestamp__year=year).order_by('timestamp')
-    response = {'tracks': tracks}
 
-    return render(request,'map.html', response)
+    context = {
+        'tracks': tracks,
+        'tiles_url': settings.TILES_URL,
+        'tiles_opt': settings.TILES_OPT,
+        'track_color': settings.TRACK_COLOR,
+    }
+
+    return render(request, 'map.html', context)
